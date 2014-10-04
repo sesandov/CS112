003d5e1045d5845ab1c6ec3314b98c594f33782b
sesandov <s.sandoval5991@gmail.com>
2014-10-04 12:44:51 -0700

    Chapter 3 excercises.

---
+++ CS112/src/ch3Excercises.java
@@ -0,0 +1,46 @@
+
+public class ch3Excercises
+{
+	3.1-
+		a) public
+		b) class
+		c) new
+		d) type, variable name
+		e) package
+		f) object
+		g) float, double
+		h) 7
+		i) never
+		j) to the class
+		k) void
+		l) always
+		m) java lang
+		n) .
+		o) float
+		p) 4 
+		q) %f
+		r) primitive, reference
+		
+	3.2-
+		a) True
+		b) True
+		c) True
+		d) True 
+		e) False
+		f) False
+		g) True
+		h) False
+		i) False
+		j) True
+		k) True 	
+		l) True
+	3.3-
+		Local variables can only be used in a method of a class. While
+		field variables can be used throughout the whole class itself, or 
+		multiple methods.
+	3.4-
+		Parameters are used when you define the method. Arguments is the info
+		that is passed to the method as a 'parameter'. 
+		
+	
+}
